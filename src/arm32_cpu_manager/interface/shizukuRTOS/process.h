6a75502afb70e06243cac1cd83537d47ffab041c
YUGA <113231660+danfunc@users.noreply.github.com>
2023-06-17 10:51:44 +0900

    初期化

--- src/arm32_cpu_manager/interface/shizukuRTOS/process.h
+++
@@ -1,7 +0,0 @@
-#ifndef SHIZUKU_RTOS_PROCESS
-#define SHIZUKU_RTOS_PROCESS
-#include "shizukuRTOS/context.h"
-void new_process(void (*entrypoint)(void), int argc, char *argv[]);
-void shizuku_RTOS_contextSwich();
-void add_process(shizuku_RTOS_context context);
-#endif
--- src/fatalError/include/shizukuRTOS/fatalError.h
+++
@@ -1,4 +0,0 @@
-#ifndef SHIZUKU_RTOS_FATAL_ERROR
-#define SHIZUKU_RTOS_FATAL_ERROR
-void fatalError(char errorMessage[]);
-#endif
